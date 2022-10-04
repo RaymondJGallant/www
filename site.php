3ba783bd725f782ed1824a404dded807e25547eb
raymondjgallant <raymondgallant@mesalands.edu>
2022-10-03 20:08:07 -0600

    follow php tutorial

---
+++ site.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesalands 3 Belongs To Me</title>
+</head>
+<body>
+    <?php
+    echo "<p>This Is My Site</p>"
+    echo "<h1> Mesalands 3 Belongs To Me</h1>";
+    echo "<hr>";
+    
+    ?>
+    <?php
+        echo "There once was a man named John<br>";
+        echo "He was 35 years old<br>";
+        echo "He really liked the name John<br>";
+        echo "But he didn't like being 35<br>";
+    ?>
+      
+    
+</body>
+</html>

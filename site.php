76d899bface9cc508e248831b623b605643ac315
raymondjgallant <raymondgallant@mesalands.edu>
2022-10-05 16:19:47 -0600

    change $GET to $POST for security

--- site.php
+++ site.php
@@ -7,32 +7,19 @@
     <title>Mesalands 3 Belongs To Me</title>
 </head>
 <body>
-    
-   
+    <form action="site.php" method="post">
+   Password: <input type= "password" name="password"><br>
+    <input type="submit">
+    </form>
+    <br><br>
 
 
+    <?php
+    //$GET vs $POST
 
-<form action="site.php" method ="get">
-        Color:<input type="text" name="color"> <br>
-        Plural Noun:<input type="text" name="pluralNoun"> <br>
-        Celebrity:<input type="text" name="celebrity"> <br>
-        <input type ="submit">
-</form>
-<br><br>
-
-            <?php
-            $color = $_GET["color"];
-            $pluralNoun = $_GET["pluralNoun"];
-            $celebrity = $_GET["celebrity"];
-
-
-            echo "Roses are $color <br>";
-            echo "$pluralNoun are blue <br>";
-            echo "I love $celebrity <br>";
-            ?>
-
-
-
+        echo $_POST["password"];
+       
+    ?>
 
 
 

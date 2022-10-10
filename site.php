e775851f9d608dffc54111727402f8a21b3e3efa
raymondjgallant <raymondgallant@mesalands.edu>
2022-10-10 17:18:51 -0600

    building a calculator

--- site.php
+++ site.php
@@ -7,19 +7,7 @@
     <title>Mesalands 3 Belongs To Me</title>
 </head>
 <body>
-    <?php
-    function getMax($num1, $num2, $num3){
-        if($num1 >= $num2 && $num1>= $num3){
-            return $num1;
-        } elseif($num2 >= $num1 && $num2 >=$num3){
-            return $num2;
-        }else{
-            return $num3;
-        }
-    }
-
-        echo getMax(300,90,400);
-    ?>
+    
 
    
 </body>
